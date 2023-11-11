3ada41fb2e2373f6feaadfe9441803bcb5372c00
Baez Pamela <baezpamelaj@gmail.com>
2023-11-11 17:16:01 -0300

    agrego middleware

--- servidorExpress.js
+++ servidorExpress.js
@@ -1,11 +1,62 @@
 const express = require('express');
-const app = express();
+const server = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+/*funciones*/
+function validarUsuario(req ,res ,next) {
+    const usuarioValido = true;
+    if(usuarioValido){
+        next();
+    }else{
+        res.status(401).send('Elusuariono es valido');   
+    }
+}
+
+/*middlewares */
+//server.use(validarUsuario); //comentado porque afecta a todas las rutas
+        server.use(express.static('public'));
+    
+
+    /* defino rutas*/
+server.get('/', (req, res) => {
+        res.send('hola mundo!');
+
+    });
+    server.post('/formulario',(req,res) => { 
+        res.send('se envio formulario');
+    });
+    server.get('/panel',[validarUsuario] ,(req, res) => {
+        res.send('Bienvenido al panel');
+    });
+       
+    server.get('/buscarUsuario',(req,res) => { 
+    const encontrado= true;
+    
+    if (encontrado) {
+        res.status(200).send('status 200: El usuario fue encontrado');
+
+    }else{
+        res.status(404).send('status 404:El usuario no se encontro');
+    }
+});
+
+server.get('/200',(req, res) => {
+
+    res.status(200).send('status 200:ok');
 })
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+server.get('/301',(req, res) => {
+
+    res.status(301).send('status 301: la paina se traslado a otro lugar de manera permanente');
+})
+
+server.get('/400',(req, res) => {
+
+    res.status(400).send('Bad request.ocurrio un error');
+})
+
+
+server.listen(PORT, () => {
+  console.log(`servidor corriendo el el puerto ${PORT}`);
 });
