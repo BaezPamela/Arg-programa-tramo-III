1fbad6ec41e13597f0dc79f859e96bc5350b0ed2
Baez Pamela <baezpamelaj@gmail.com>
2023-11-25 16:17:16 -0300

    Remove accidentally added subdirectory

---
+++ README.md
@@ -0,0 +1,1 @@
+# Arg-programa-tramo-III
---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const server = express();
+const PORT = 3000;
+
+
+
+    
+
+  
+    server.get('/', (req, res) => {
+        res.send('hola mundo!');
+
+    });
+   
+/*iniciar el servidor*/
+server.listen(PORT, () => {
+  console.log(`servidor corriendo el el puerto ${PORT}`);
+});
